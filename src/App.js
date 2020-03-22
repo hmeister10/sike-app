e93a52e27f43addb40783bcda00c25cd6acea6d8
Himanshu Hiranandani <hmeister10@gmail.com>
2020-03-22 13:14:54 +0530

    updates thee landing page adds react bootstrap

--- src/App.js
+++ src/App.js
@@ -4,44 +4,65 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.scss';
+
+// Components
 import Landing from './components/landing/landing';
 import Profile from './components/profile/profile';
 import Question from './components/question/question';
 import Results from './components/results/results';
 import Voting from './components/voting/voting';
 
+function RouteData() {
+  return (<Router>
+    <div>
+      {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/question">
+          <Question />
+        </Route>
+        <Route path="/voting">
+          <Voting />
+        </Route>
+        <Route path="/results">
+          <Results />
+        </Route>
+      </Switch>
+    </div>
+  </Router>)
+}
 
 function App() {
   return (
-    <Router>
-      <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/question">
-            <Question />
-          </Route>
-          <Route path="/voting">
-            <Voting />
-          </Route>
-          <Route path="/results">
-            <Results />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Container>
+      <Row>
+        <Col>
+          SIKE!!
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <RouteData />
+        </Col>
+      </Row>
+      <Row>
+      </Row>
+    </Container>
+
   );
 }
 
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,30 +1,68 @@
 import React from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './landing.scss';
 import DBUtils from '../../utils/DBUtils';
 
 export default class Landing extends React.Component {
 
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+      code: '',
+    }
+  }
+
   async componentWillMount() {
     const data = await DBUtils.getData('questions')
     console.log(data);
   }
 
+  handleCodeChange(e) {
+    this.setState({ code: e.target.value });
+  }
+
+  joinGame() {
+    let code = this.state.code || 0
+
+    // this checks if the code exists
+    // if it has maximum of 6 characters
+    // if it is only digits and 6 digits
+    if (this.state.code && code.length === 6 && code.match(/\d{6}/)) {
+      // join the game
+      // GameUtils.joinGame(code)
+    } else {
+      // Show error
+    }
+
+  }
+
+  newGame() {
+    console.log('Start a new game')
+  }
+
   render() {
     return (
-      <div className="landing-container">
-        SIKE!!!
-        <ul>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/question">START NEW GAME</Link>
-
-          </li>
-        </ul>
+      <div>
+        <Row className="landing-container">
+          <Col>
+            <input
+              type="text"
+              maxLength="6"
+              value={this.state.code}
+              onChange={this.handleCodeChange.bind(this)}
+              placeholder="Enter a 6 digit code..." />
+            <Button variant="primary" onClick={this.joinGame.bind(this)}>Join Game</Button>
+          </Col>
+        </Row>
+        <Row className="landing-container">
+          <Col>
+            <Button variant="primary" onClick={this.newGame.bind(this)}>Start New Game</Button>
+          </Col>
+        </Row>
 
       </div>
+
     );
   }
 }
